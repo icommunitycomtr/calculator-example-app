9a36943ac531269a1d01a7e8ce30ff2ca5c81045
gulzade <gulzadekrts@gmail.com>
2025-01-09 23:35:08 +0300

    project created

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Gülzade Karataş on 9.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
