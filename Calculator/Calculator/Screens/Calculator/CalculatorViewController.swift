5bfa27cc3a9a64e20bdf3b2b53f02bfcf807da9f
gulzade <gulzadekrts@gmail.com>
2025-01-11 07:19:41 +0300

    Added operations for clearButton and numberPressed

--- Calculator/Calculator/Screens/Calculator/CalculatorViewController.swift
+++ Calculator/Calculator/Screens/Calculator/CalculatorViewController.swift
@@ -10,33 +10,50 @@ import UIKit
 class CalculatorViewController: UIViewController {
     
     @IBOutlet weak var calculatorLabel: UILabel!
+    
+    var currentValue: String = "0"
+    var previousValue: Double = 0
+    var currentOperation: String? = nil
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     
     
-    @IBAction func numberPressed(_ sender: Any) {
+    @IBAction func numberPressed(_ sender: UIButton) {
+        if currentValue == "0" {
+            currentValue = sender.titleLabel?.text ?? "0"
+        }else{
+            currentValue += sender.titleLabel?.text ?? ""
+        }
+        calculatorLabel.text = currentValue
     }
     
     
     
-    @IBAction func operationPressed(_ sender: Any) {
+    @IBAction func operationPressed(_ sender: UIButton) {
     }
     
     
     
     
-    @IBAction func equalButton(_ sender: Any) {
+    @IBAction func equalButton(_ sender: UIButton) {
     }
     
-    @IBAction func clearButton(_ sender: Any) {
+    
+    @IBAction func clearButton(_ sender: UIButton) {
+        currentValue = "0"
+        calculatorLabel.text = currentValue
+        previousValue = 0
+        currentOperation = nil
     }
     
-    @IBAction func percentButton(_ sender: Any) {
+    @IBAction func percentButton(_ sender: UIButton) {
     }
     
-    @IBAction func signChangeButton(_ sender: Any) {
+    @IBAction func signChangeButton(_ sender: UIButton) {
     }
     
 
